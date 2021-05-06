f9afb61f8933514a23e60d6cb7ee796bcaabf6d1
JRMDEEz <dryestboot@gmail.com>
2021-05-06 22:34:06 +0800

    3 files modified

--- app/app.routing.module.ts
+++ app/app.routing.module.ts
@@ -1,17 +1,17 @@
-import { RouterModule } from "@angular/router";
-import { NgModule } from "@angular/core";
-import { ContactViewComponent } from "./views/contact/contact.component";
-import { HomeViewComponent } from "./views/home/home.component";
-import { KnowledgeViewComponent } from "./views/knowledge/knowledge.component";
-import { ProjectsViewComponent } from "./views/projects/projects.component";
-import { NotFoundViewComponent } from "./views/notfound/notfound.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { PrivacyPolicyViewComponent } from "./views/privacypolicy/privacypolicy.component";
-import { ViewComponent } from "./views/view/view.component";
-import { AccountViewComponent } from "./views/account/account.component";
-import { EditorViewComponent } from "./views/editor/editor.component";
-import { BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { ModalUploadComponent } from './modals/upload/upload.component';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ContactViewComponent } from './views/contact/contact.component';
+import { HomeViewComponent } from './views/home/home.component';
+import { KnowledgeViewComponent } from './views/knowledge/knowledge.component';
+import { ProjectsViewComponent } from './views/projects/projects.component';
+import { NotFoundViewComponent } from './views/notfound/notfound.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { PrivacyPolicyViewComponent } from './views/privacypolicy/privacypolicy.component';
+import { ViewComponent } from './views/view/view.component';
+import { AccountViewComponent } from './views/account/account.component';
+import { EditorViewComponent } from './views/editor/editor.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalUploadComponent } from './views/modals/upload/upload.component';
 import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [
@@ -24,24 +24,25 @@ import { MaterialModule } from './material.module';
     ViewComponent,
     AccountViewComponent,
     EditorViewComponent,
-    ModalUploadComponent,MaterialModule
+    ModalUploadComponent,
+    MaterialModule
   ],
   entryComponents: [ModalUploadComponent],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     RouterModule.forRoot([
-      { path: "", pathMatch: "full", redirectTo: "home" },
-      { path: "home", component: HomeViewComponent },
-      { path: "contact", component: ContactViewComponent },
-      { path: "projects", component: ProjectsViewComponent },
-      { path: "privacypolicy", component: PrivacyPolicyViewComponent },
-      { path: "knowledge", component: KnowledgeViewComponent },
-      { path: "404", component: NotFoundViewComponent },
-      { path: "projects/view", component: ViewComponent },
-      { path: "projects/edit", component: EditorViewComponent },
-      { path: "account", component: AccountViewComponent },
-      { path: "**", redirectTo: "404" }
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'home', component: HomeViewComponent },
+      { path: 'contact', component: ContactViewComponent },
+      { path: 'projects', component: ProjectsViewComponent },
+      { path: 'privacypolicy', component: PrivacyPolicyViewComponent },
+      { path: 'knowledge', component: KnowledgeViewComponent },
+      { path: '404', component: NotFoundViewComponent },
+      { path: 'projects/view', component: ViewComponent },
+      { path: 'projects/edit', component: EditorViewComponent },
+      { path: 'account', component: AccountViewComponent },
+      { path: '**', redirectTo: '404' }
     ])
   ],
   exports: [RouterModule],
